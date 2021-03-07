4d5ba21fbecfaf8d6795bb885715bc9aae1acb14
mengjelee <43258760+mengjelee@users.noreply.github.com>
2021-03-07 23:12:30 +0800

    210307

--- login/settings.py
+++ login/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = '(6@zgd+736!4r+tk4te28$)r3ujr2vkbgfx^lzg8%(6maf%*gg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0','ecpay6.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0','ecpay7.herokuapp.com','127.0.0.1']
 
 
 # Application definition
